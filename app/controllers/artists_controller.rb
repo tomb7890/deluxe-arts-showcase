89ce0af31de974abd25a4a925526f29baf026b59
Tom Billingsley <tom.billingsley@gmail.com>
2021-10-27 07:18:29 -0400

    Link to artist_path and sketch out initial view

--- app/controllers/artists_controller.rb
+++ app/controllers/artists_controller.rb
@@ -3,4 +3,14 @@ class ArtistsController < ApplicationController
   def index
     @artists = Artist.all
   end
+
+  def show
+    @artist = Artist.find(params[:id])
+  end
+
+
+  private
+  def artist_params
+    params.require(:artist).permit(:name, :description)
+  end
 end
