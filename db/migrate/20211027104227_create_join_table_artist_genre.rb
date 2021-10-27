1f377d8361d8f8359f83f10b19e8acbc3c2ca1c2
Tom Billingsley <tom.billingsley@gmail.com>
2021-10-27 06:44:15 -0400

    rails g migration CreateJoinTableArtistGenre artist genre

---
+++ db/migrate/20211027104227_create_join_table_artist_genre.rb
@@ -0,0 +1,8 @@
+class CreateJoinTableArtistGenre < ActiveRecord::Migration[6.1]
+  def change
+    create_join_table :artists, :genres do |t|
+      t.index [:artist_id, :genre_id]
+      t.index [:genre_id, :artist_id]
+    end
+  end
+end
