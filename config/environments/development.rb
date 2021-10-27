2f4e1d5f610595132a3bf0c54fe092696e9a6b71
Tom Billingsley <tom.billingsley@gmail.com>
2021-10-27 17:02:04 -0400

    Fix the SAC logo

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -74,5 +74,10 @@ Rails.application.configure do
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
 
-  config.hosts << "fractaldm.local" 
+  config.hosts << "fractaldm.local"
+
+  # Must include to get inline SVGs to work in deploy
+  config.assets.css_compressor = :sass
+
+  
 end
